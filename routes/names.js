18eb9d2a114aa7a762da8519576c24a7164a61a9
RJ Salvador <rjsalvadorr@gmail.com>
2019-03-23 20:08:39 -0400

    Created endpoints for random elf and dwarf names

--- routes/names.js
+++ routes/names.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-var mocks = require('../utils/mocks');
-var shuffle = require('lodash/shuffle');
-var shuffle = require('lodash/shuffle');
+const mocks = require('../utils/mocks');
+const shuffle = require('lodash/shuffle');
 
 /* GET names listing. */
 router.get('/', function(req, res, next) {
@@ -14,24 +13,40 @@ router.get('/', function(req, res, next) {
 router.get('/dwarf/:numNames', function(req, res, next) {
   let dorfNames = mocks.getDwarfNames();
   let numNames = parseInt(req.params.numNames, 10);
-  console.log(req.params);
-  console.log(numNames);
-
 
   // if numNames is a valid number,
   // and it's smaller than the dorfNames length,
   // trim result size to numNames
   if(numNames && numNames < dorfNames.length) {
-    console.log("Dorf names before: " + JSON.stringify(dorfNames));
-    dorfNames = dorfNames.slice(numNames - 1);
-    console.log("Dorf names after: " + JSON.stringify(dorfNames));
+    dorfNames = shuffle(dorfNames).slice(dorfNames.length - numNames);
   }
 
   res.json(dorfNames);
 });
 
+/* GET dwarf names listing */
+router.get('/dwarf', function(req, res, next) {
+  const dorfNames = mocks.getDwarfNames();
+  res.json(dorfNames);
+});
+
 /* GET names listing. */
 router.get('/elf/:numNames', function(req, res, next) {
+  let elfNames = mocks.getElfNames();
+  let numNames = parseInt(req.params.numNames, 10);
+  
+  // if numNames is a valid number,
+  // and it's smaller than the elfNames length,
+  // trim result size to numNames
+  if(numNames && numNames < elfNames.length) {
+    elfNames = shuffle(elfNames).slice(elfNames.length - numNames);
+  }
+
+  res.json(elfNames);
+});
+
+/* GET names listing. */
+router.get('/elf', function(req, res, next) {
   const elfNames = mocks.getElfNames();
   res.json(elfNames);
 });
