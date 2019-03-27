8f05cd3b4910b7e787e25cb1791d8d3286aa4b8d
RJ Salvador <rjsalvadorr@gmail.com>
2019-03-27 16:28:35 -0400

    Added gender toggle to names API

--- routes/names.js
+++ routes/names.js
@@ -10,6 +10,12 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET dwarf names listing */
+router.get('/dwarf', function(req, res, next) {
+  const dorfNames = mocks.getDwarfNames();
+  res.json(dorfNames);
+});
+
+/* GET dwarf names listing */
 router.get('/dwarf/:numNames', function(req, res, next) {
   let dorfNames = mocks.getDwarfNames();
   let numNames = parseInt(req.params.numNames, 10);
@@ -25,9 +31,26 @@ router.get('/dwarf/:numNames', function(req, res, next) {
 });
 
 /* GET dwarf names listing */
-router.get('/dwarf', function(req, res, next) {
-  const dorfNames = mocks.getDwarfNames();
+router.get('/dwarf/:gender/:numNames', function(req, res, next) {
+  let numNames = parseInt(req.params.numNames, 10);
+  let dorfGender = req.params.gender.toLowerCase();
+  let dorfNames = mocks.getDwarfNames(dorfGender);
+
+  // if numNames is a valid number,
+  // and it's smaller than the dorfNames length,
+  // trim result size to numNames
+  if(numNames && numNames < dorfNames.length) {
+    dorfNames = shuffle(dorfNames).slice(dorfNames.length - numNames);
+  }
+
   res.json(dorfNames);
+});
+
+
+/* GET names listing. */
+router.get('/elf', function(req, res, next) {
+  const elfNames = mocks.getElfNames();
+  res.json(elfNames);
 });
 
 /* GET names listing. */
@@ -46,8 +69,18 @@ router.get('/elf/:numNames', function(req, res, next) {
 });
 
 /* GET names listing. */
-router.get('/elf', function(req, res, next) {
-  const elfNames = mocks.getElfNames();
+router.get('/elf/:gender/:numNames', function(req, res, next) {
+  let numNames = parseInt(req.params.numNames, 10);
+  let elfGender = req.params.gender.toLowerCase();
+  let elfNames = mocks.getElfNames(elfGender);
+  
+  // if numNames is a valid number,
+  // and it's smaller than the elfNames length,
+  // trim result size to numNames
+  if(numNames && numNames < elfNames.length) {
+    elfNames = shuffle(elfNames).slice(elfNames.length - numNames);
+  }
+
   res.json(elfNames);
 });
 
